dfbf4408ee4cd0063088b4c127dc137cc3717266
Edwin Mesa <edwinfmesa@hotmail.com>
2012-06-19 18:48:43 -0500

    .agregando el gitignore

--- django_test/settings.py
+++ django_test/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'edwinfmesa',                      # Or path to database file if using sqlite3.
+        'NAME': 'django_test',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    "first_app",
 )
 
 # A sample logging configuration. The only tangible logging
