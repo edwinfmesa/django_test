2f17e4a6fa60b66c9d5fb833d340961c733c7fa0
Edwin Mesa <edwinfmesa@hotmail.com>
2012-06-26 16:28:15 -0500

    Funciona localmente

--- django_test/urls.py
+++ django_test/urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(.*)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT}),
 )
 
 
@@ -28,9 +29,3 @@ urlpatterns = patterns('',
 #            'django.views.static.serve',
 #            {'document_root' : settings.STATIC_ROOT }),)
 #    
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': settings.MEDIA_ROOT,
-        }),
-    )
