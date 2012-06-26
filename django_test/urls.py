03e64ecb6b2621d35cec3781cced28946bf0bcad
Edwin Mesa <edwinfmesa@hotmail.com>
2012-06-26 08:40:40 -0500

    quitando el settings.py

--- django_test/urls.py
+++ django_test/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 #from django.conf.urls.defaults import *
 from first_app.views import mostrar, home
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -16,3 +17,20 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+
+# tus urlpatterns de toda la vida
+#
+#if settings.DEBUG and settings.STATIC_ROOT:
+#    urlpatterns += patterns('',
+#        (r'%s(?P<path>.*)$' % settings.STATIC_URL.lstrip('/'), 
+#            'django.views.static.serve',
+#            {'document_root' : settings.STATIC_ROOT }),)
+#    
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )
